5d9339d129b1e6782434eb069ce88f39f387c33d
Xavier T``` <xavier-tan@hotmail.sg>
2021-02-05 01:00:15 -0800

    need to fix image change if else conds

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,37 @@
 // main.js
 
 // TODO
+var volNum = document.getElementById("volume-number");
+var volSlider = document.getElementById("volume-slider");
+var volImage = document.getElementById("volume-image");
+
+function setVolume(vol) {
+    changeVolImg(vol);
+
+    return function() {
+      vol.value = this.value.toString();
+    }
+}
+
+function changeVolImg() {
+  // else {
+  //   volImage.src = "./assets/media/icons/volume-level-0.svg";
+  // }
+  if (this.value >= 67 && this.value <= 100) {
+    volImage.src = "./assets/media/icons/volume-level-3.svg";
+  }
+  else if (this.value >= 34 && this.value <= 66) {
+    volImage.src = "./assets/media/icons/volume-level-2.svg";
+  }
+  else if (this.value >= 0 && this.Value <= 33) {
+    volImage.src = "./assets/media/icons/volume-level-1.svg";
+  }
+  else if (this.value == 0) {
+    volImage.src = "./assets/media/icons/volume-level-0.svg";
+  }
+}
+
+volSlider.addEventListener("change", setVolume(volNum));
+volNum.addEventListener("change", setVolume(volSlider));
+volSlider.addEventListener("change", changeVolImg);
+volNum.addEventListener("change", changeVolImg);
