811e560c3449cc8734db087fdb6bd139d8c8e633
Xavier T``` <xavier-tan@hotmail.sg>
2021-02-05 16:11:26 -0800

    almost done w/ button just need disable button at 0

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -4,34 +4,63 @@
 var volNum = document.getElementById("volume-number");
 var volSlider = document.getElementById("volume-slider");
 var volImage = document.getElementById("volume-image");
+var airHorn = document.getElementById("radio-air-horn");
+var carHorn = document.getElementById("radio-car-horn");
+var partyHorn = document.getElementById("radio-party-horn");
+var honkBtn = document.getElementById("honk-btn");
+var audio = document.getElementById("horn-sound");
+var hornForm = document.getElementById("party-horn-form");
 
-function setVolume(vol) {
-    changeVolImg(vol);
+airHorn.addEventListener("click", (e) => {
+  audio.src = "./assets/media/audio/air-horn.mp3";
+})
 
-    return function() {
-      vol.value = this.value.toString();
-    }
-}
+carHorn.addEventListener("click", (e) => {
+  audio.src = "./assets/media/audio/car-horn.mp3";
+});
 
-function changeVolImg() {
-  // else {
-  //   volImage.src = "./assets/media/icons/volume-level-0.svg";
-  // }
-  if (this.value >= 67 && this.value <= 100) {
+partyHorn.addEventListener("click", (e) => {
+  audio.src = "./assets/media/audio/party-horn.mp3";
+});
+
+hornForm.addEventListener("submit", function(e) {
+  e.preventDefault();
+  audio.play();
+});
+
+volSlider.addEventListener("click", (e) => {
+  volNum.value = e.target.value;
+  audio.volume = e.target.value / 100;
+
+  if (e.target.value >= 67 && e.target.value <= 100) {
     volImage.src = "./assets/media/icons/volume-level-3.svg";
   }
-  else if (this.value >= 34 && this.value <= 66) {
+  else if (e.target.value >= 34 && e.target.value <= 66) {
     volImage.src = "./assets/media/icons/volume-level-2.svg";
   }
-  else if (this.value >= 0 && this.Value <= 33) {
+  else if (e.target.value > 0 && e.target.value < 34) {
     volImage.src = "./assets/media/icons/volume-level-1.svg";
   }
-  else if (this.value == 0) {
+  else if (e.target.value == 0) {
     volImage.src = "./assets/media/icons/volume-level-0.svg";
   }
-}
 
-volSlider.addEventListener("change", setVolume(volNum));
-volNum.addEventListener("change", setVolume(volSlider));
-volSlider.addEventListener("change", changeVolImg);
-volNum.addEventListener("change", changeVolImg);
+});
+
+volNum.addEventListener("click", (e) => {
+  volSlider.value = e.target.value;
+  audio.volume = e.target.value / 100;
+
+  if (e.target.value >= 67 && e.target.value <= 100) {
+    volImage.src = "./assets/media/icons/volume-level-3.svg";
+  }
+  else if (e.target.value >= 34 && e.target.value <= 66) {
+    volImage.src = "./assets/media/icons/volume-level-2.svg";
+  }
+  else if (e.target.value > 0 && e.target.Value < 34) {
+    volImage.src = "./assets/media/icons/volume-level-1.svg";
+  }
+  else if (e.target.value == 0) {
+    volImage.src = "./assets/media/icons/volume-level-0.svg";
+  }
+});
